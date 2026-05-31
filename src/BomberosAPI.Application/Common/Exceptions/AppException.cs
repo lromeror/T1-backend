530fee94ddec3f810db971882ce13cf30effce31
Luis F. Romero <lromeror@fiec.espol.edu.ec>
2026-05-30 21:35:46 -0500

    Issue 16

---
+++ src/BomberosAPI.Application/Common/Exceptions/AppException.cs
@@ -0,0 +1,11 @@
+namespace BomberosAPI.Application.Common.Exceptions;
+
+public abstract class AppException : Exception
+{
+    public int StatusCode { get; }
+
+    protected AppException(string message, int statusCode) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
